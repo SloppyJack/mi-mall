9abca7996d16a392404593941cc82a0b99336742
SloppyJack <312883327@qq.com>
2020-05-27 22:59:07 +0800

    初始化项目结构，初步构建了vue-router

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>我是底部</div>
+</template>
+
+<script>
+    export default {
+        name: 'nav-footer'
+    }
+</script>
---
+++ src/pages/order.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+</template>
+
+<script type="text/ecmascript-6">
+    import NavHeader from '../components/NavHeader'
+    import NavFooter from "../components/NavFooter"
+
+    export default {
+        name:'order',
+        components: {
+            NavHeader,
+            NavFooter
+        }
+    }
+</script>
